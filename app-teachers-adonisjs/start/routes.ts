35af4c9d97e185e2f119115469db46e643902ae7
Kiril Bormin <borminnkiril@gmail.com>
2026-01-14 16:18:21 +0100

    feat (exercice) : step 4

--- app-teachers-adonisjs/start/routes.ts
+++ app-teachers-adonisjs/start/routes.ts
@@ -6,7 +6,8 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
+import TeachersController from '#controllers/teachers_controller'
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.get('/', [TeachersController, 'index']).as('home')
